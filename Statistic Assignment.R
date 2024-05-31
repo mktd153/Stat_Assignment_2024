63b21d02332624b59721f13a87c847a8f838fdf3
mktd153 <mktd.153@gmail.com>
2024-05-31 13:25:58 +0200

    Update Statistic Assignment.R

--- Statistic Assignment.R
+++ Statistic Assignment.R
@@ -1,28 +1,30 @@
-# i'm directly pulling data from the FRED so i need to install a few new packages
+# we're directly pulling data from the FRED so we need to install a few new packages first
 install.packages ("fredr")
 install.packages ("tidyverse")
 library(dplyr)
-library(fredr)
 library(tidyverse)
+library(fredr)
 
-#i used my own api key and you can request it from the FRED website 
-## https://fredaccount.stlouisfed.org/apikeys
+
+# Question 1 & 2
+## we used our own api key and you can request it from the FRED website
+### https://fredaccount.stlouisfed.org/apikeys
 fredr_set_key ("467279e44c3e723a9fa904ea4abc907e")
 
-#draw data from FRED using the code and then drawing using the series ID
+# draw data from FRED using the code and then drawing using the series ID
 test <- fredr_series_search_text("sp500")
 sp500_raw <- fredr_series_observations(series_id = "SP500"); head(sp500_raw)
 
-#tidy up the data by renaming and removing unnecessary columns
+# tidy up the data by renaming and removing unnecessary columns
 colnames(sp500_raw)[3] <- "prices"
 sp500_raw1 <- subset(sp500_raw, select = -c(realtime_start,realtime_end,series_id) )
 
-##tidying by also removing the NA values
+## tidying by also removing the NA values
 sp500_raw2 <- sp500_raw1 %>% filter(!is.na(prices))
 head(sp500_raw2)
 
-### i am using the data from the 1 Dec 2019 until 31 Jan 2024
-#### so i'm removing the uncessary rows 
+### we are using the data from the 1 Dec 2019 until 31 Jan 2024
+#### so we are removing the uncessary rows
 sp500_raw3  <- sp500_raw2 %>% filter( date >= "2019-12-01")
 sp500       <- sp500_raw3 %>% filter( date <= "2024-01-31")
 head(sp500)
@@ -33,24 +35,75 @@ ggplot (sp500, aes(x=date, y=prices))+
   labs (title="Time Series SP500 from Dec 2019 to Jan 2024",
         y="Closing Price of Index",
         x= "Date",
-        caption = "FRED ID: SP500")
+        caption = "Fig. 1")
 
-#differencing the graph with a lag of 1
-sp500_diff1 <-  sp500$prices[(2):length(sp500$prices)]  - 
-                sp500$prices[1:length(sp500$prices)-1]
-##we actually don't need a data frame but i prefer to visualise it with a table
-sp500_diff <- data.frame (
-  time = c(1:length(sp500_diff1)),
-  prices = sp500_diff1
-)
+
+# Question 3
+## differencing the log of the Prices with a backshift operator of 1
+Rt <-  log10(sp500$prices[(2):length(sp500$prices)])  -
+  log10(sp500$prices[1:length(sp500$prices)-1])
+
+###we prefer to see the data in a table/frame
+sp500_difflog <- data.frame (
+  time = c(1:length(Rt)),
+  prices = Rt
+); head(sp500_difflog)
 
 #plotting the data frame for the difference
-ggplot ( sp500_diff, aes(x = time, y = prices))+
+ggplot ( sp500_difflog, aes(x = time, y = prices))+
   geom_line (color="black")+
-  labs (title="Time Series SP500 from Dec 2019 to Jan 2024",
-        y="difference of prices with lag,t = 1",
-        x= "time",
-        caption = "FRED ID: SP500")
+  labs (title="Difference of Log. of Prices with a Backshift Operator = 1",
+        y="difference of prices, delta p ",
+        x= "time, t",
+        caption = "Fig. 2")
+
+
+# Question 4
+## histogram with Prices
+ggplot(data = sp500, aes(x = prices)) +
+  geom_histogram(bins=200)+
+  labs ( title = "Histogram of the Prices",
+         x = " prices, p",
+         caption = "Fig. 3")
+
+## histogram with differences of log of prices
+ggplot(data = sp500_difflog, aes(x = prices)) +
+  geom_histogram(bins=200)+
+  labs ( title = "Histogram of the Difference of Log of Prices",
+         x = "diff. log prices, delta p",
+         caption = "Fig. 4")
+
+
+# Question 5
+## Calculating the mean of the dataset sp500_difflog1
+m <- mean (Rt)
+v <- var ( Rt, na.rm = FALSE)
+
+ecdf_func <- ecdf(Rt)
+
+data_ecdf_cdf <- data.frame (
+                x_1 = ecdf_func(Rt),
+                y_1 = pnorm(Rt, mean = m, sd = sqrt(v))
+                )
+
+ggplot() +
+  geom_point( data = data_ecdf_cdf, aes( x = x_1, y = y_1 ), color ="black", size = 0.5)+
+  geom_abline(slope = 1, intercept = 0, color = "red", linetype = "dashed", size = 0.5)+
+  labs (title="CDF vs ECDF",
+        y= "Cumulative Distribution Function, cdf",
+        x= "Empirical Distribution Function, ecdf",
+        caption = "Fig. 5")
+
+# Question 6
+## with mu = 0 confidence level 99%
+t.test (Rt, mu= 0,
+        alternative = c("two.sided"),
+        conf.level = 0.99)
+
+## with mu = mean(Rt), confidence level 95%
+t.test (Rt, mu = mean(Rt) ,
+        alternative = c("two.sided"),
+        conf.level = 0.95)
 
 
 
